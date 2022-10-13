c5e7515a7f45ab22a36991dbb2e92f94b59d65e9
Johnathan <johnathanferreira02@gmail.com>
2022-10-13 11:35:18 -0300

    SellerService and basic FindAll

--- SalesWebMvc/Data/SeedingService.cs
+++ SalesWebMvc/Data/SeedingService.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore.Internal;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using SalesWebMvc.Models;
 using SalesWebMvc.Models.Enums;
-using System;
 
 namespace SalesWebMvc.Data
 {
@@ -16,9 +18,11 @@ namespace SalesWebMvc.Data
 
         public void Seed()
         {
-            if (_context.Department.Any() || _context.Seller.Any() || _context.SalesRecord.Any()) 
+            if (_context.Department.Any() ||
+                _context.Seller.Any() ||
+                _context.SalesRecord.Any())
             {
-                return;
+                return; // DB has been seeded
             }
 
             Department d1 = new Department(1, "Computers");
@@ -68,14 +72,13 @@ namespace SalesWebMvc.Data
 
             _context.Seller.AddRange(s1, s2, s3, s4, s5, s6);
 
-            _context.SalesRecord.AddRange(r1, r2, r3, r4, r5, r6, r7, r8, r9, r10,
+            _context.SalesRecord.AddRange(
+                r1, r2, r3, r4, r5, r6, r7, r8, r9, r10,
                 r11, r12, r13, r14, r15, r16, r17, r18, r19, r20,
                 r21, r22, r23, r24, r25, r26, r27, r28, r29, r30
-             );
+            );
 
             _context.SaveChanges();
-        
         }
-
     }
 }
